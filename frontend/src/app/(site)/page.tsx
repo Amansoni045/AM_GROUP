0a17911a99552270b2eb4e10f91c2b713f6d832c
tani066 <184628624+tani066@users.noreply.github.com>
2026-08-14 20:24:52 +0530

    removed

--- frontend/src/app/(site)/page.tsx
+++ frontend/src/app/(site)/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
       <WhatWeDo />
       <Services />
       <IndustryExpertise />
-      <Clients />
+      {/* <Clients /> */}
       <ConsultationForm />
     </main>
   );
